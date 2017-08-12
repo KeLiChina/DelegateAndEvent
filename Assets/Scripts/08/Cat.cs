d68af9ed6afe345f33eef6ca1502ca42bb12a90a
24967 <forestlii@outlook.com>
2017-08-12 22:00:02 +0800

    finish08

---
+++ Assets/Scripts/08/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Master.instance.eatDel +=CatEat;
+	}
+	
+	private void CatEat()
+	{
+		Debug.Log("Master Allow Cat Eat,Cat Crow MiaoMiaoMiao And Run");
+	}
+}
