1264c19a982c2edc075dd3274552c63f9801423f
24967 <forestlii@outlook.com>
2017-08-12 21:22:46 +0800

    finish07

---
+++ Assets/Scripts/07/WebInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebInfo : MonoBehaviour {
+
+	
+	void Start () {
+		// ShowWeb(Baidu);
+		// Hello("MK");
+		string name;
+		name = "MK";
+		name = "C#";
+		Debug.Log(name);
+		WebInfoDel webInfoDel;
+		webInfoDel = Baidu;
+		webInfoDel += Ali;
+		webInfoDel += MKCode;
+		webInfoDel -=Baidu;
+		webInfoDel();
+	}
+	private delegate void WebInfoDel ();
+	private void Baidu()
+	{
+		Debug.Log("Baidu.com");
+	}
+	private void Ali()
+	{
+		Debug.Log("Ali.com");
+	}
+	private void MKCode()
+	{
+		Debug.Log("MKCode.com");
+	}
+
+	private void ShowWeb(WebInfoDel web)
+	{
+		web();
+	}
+	private void Hello(string name)
+	{
+		Debug.Log("Hello" + name);
+	}
+}
