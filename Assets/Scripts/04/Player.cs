7c29cf46838ca465db9ae5419d8cba65f41346f4
24967 <forestlii@outlook.com>
2017-08-12 16:56:47 +0800

    finish04

---
+++ Assets/Scripts/04/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(MapManager.instance.count);
+		MapManager.instance.count = 100;
+		Debug.Log(MapManager.instance.count);
+		Debug.Log(XMLManager.Instance.name + ">>>XML") ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/04/XMLManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XMLManager  {
+	public string name = "like";
+	private static XMLManager instance;
+	private XMLManager(){}
+	public static XMLManager Instance
+	{
+		get
+		{
+			if ( instance == null)
+			{
+				instance = new XMLManager();
+			}
+			return instance;
+		}
+	}
+}
