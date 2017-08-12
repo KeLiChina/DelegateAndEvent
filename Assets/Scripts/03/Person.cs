25e30c9c470aa58e787a9d51d210b69e8b5be260
24967 <forestlii@outlook.com>
2017-08-12 16:17:34 +0800

    finish03

---
+++ Assets/Scripts/03/Person.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person<T,K> {
+
+	public T info;
+	public int age;
+	public string name;
+	public K mes;
+	public void ShowPerson()
+	{
+		Debug.Log(string.Format("{0}---{1}---{2}----{3}",name,age,info,mes));
+	}
+	public T ShowInfo()
+	{
+		return info;
+	}
+	public T ShowName<T>(T name)
+	{
+		return name;
+	}
+}
---
+++ Assets/Scripts/03/PersonTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonTest : MonoBehaviour {
+
+	private Person<string,int> ps = new Person<string,int>();
+	private Person<float,string> ps2 = new Person<float,string>();
+	void Start () {
+		ps.age = 10;
+		ps.info = "IamKIKI";
+		ps.name = "forestli";
+		ps.mes = 5;
+		ps2.age = 10;
+		ps2.name = "li";
+		ps2.info = 5f;
+		ps2.mes = "what?";
+		ps.ShowPerson();
+		ps2.ShowPerson();
+
+		Debug.Log( ps.ShowInfo());
+		Debug.Log(ps.ShowName<string>(ps.name));
+		
+	}
+	
+
+}
