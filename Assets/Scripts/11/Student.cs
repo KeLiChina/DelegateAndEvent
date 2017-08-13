bafe867a01c327feb61bf756f7bf7d00da2ebe22
24967 <forestlii@outlook.com>
2017-08-13 22:28:21 +0800

    finish11

---
+++ Assets/Scripts/11/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Student : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MK.instance.unityDel +=QQ;
+	}
+	
+	private void QQ(string info)
+	{
+		Debug.Log(gameObject.name + " " + info);
+	}
+}
