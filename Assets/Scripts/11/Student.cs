7e01b5358961a58639f4cb9fb6c9dff4586d6b01
24967 <forestlii@outlook.com>
2017-08-13 22:55:09 +0800

    finish12

--- Assets/Scripts/11/Student.cs
+++ Assets/Scripts/11/Student.cs
@@ -6,11 +6,15 @@ public class Student : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		MK.instance.unityDel +=QQ;
+		// MK.instance.unityDel +=QQ;
+		MK.instance.unityDel += delegate(string info)
+		{
+			Debug.Log(gameObject.name + " " + info+" !!!");
+		};
 	}
 	
 	private void QQ(string info)
 	{
-		Debug.Log(gameObject.name + " " + info);
+		
 	}
 }
---
+++ Assets/Scripts/12/QQUser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QQUser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// QQGroud.instance.loginDel += Login;
+		// QQGroud.instance.messageDel += Message;
+		QQGroud.instance.loginDel += delegate(string name)
+		{
+			Debug.Log(name + " is login !!!");
+		};
+		QQGroud.instance.messageDel += delegate(string name,string message)
+		{
+			Debug.Log(name +" say: "+message +"!!!");
+		};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void Login(string name)
+	{
+		
+	}
+	private void Message(string name, string message)
+	{
+		
+	}
+}
